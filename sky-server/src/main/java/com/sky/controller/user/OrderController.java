48b2d78b0858a646cc3731c3439004e0f8433568
forget you <202368006@qq.com>
2025-11-02 17:24:31 +0800

    商家端订单管理模块和校验收货地址是否超出配送范围相关功能业务完善

--- sky-server/src/main/java/com/sky/controller/user/OrderController.java
+++ sky-server/src/main/java/com/sky/controller/user/OrderController.java
@@ -15,7 +15,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@RestController("userOrderController")
 @RequestMapping("/user/order")
 @RequiredArgsConstructor
 @Slf4j

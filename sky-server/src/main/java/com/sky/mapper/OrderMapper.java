cd13ab563e16f88500ae8c633238249907253572
forget you <202368006@qq.com>
2025-11-05 17:09:31 +0800

    营业额统计业务功能

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -10,6 +10,7 @@ import org.apache.ibatis.annotations.Update;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface OrderMapper {
@@ -66,5 +67,9 @@ public interface OrderMapper {
       */
      @Select("select * from orders where status = #{status} and order_time < #{orderTime}")
     List<Orders> getByStatusAndOrderTimeLT(Integer status, LocalDateTime orderTime);
-
+    /**
+     * 根据map统计营业额
+     * @param map
+     */
+    Double sumByMap(Map map);
 }
---
+++ sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
@@ -0,0 +1,64 @@
+package com.sky.service.impl;
+
+
+import com.sky.entity.Orders;
+import com.sky.mapper.OrderMapper;
+import com.sky.service.ReportService;
+import com.sky.vo.TurnoverReportVO;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.stereotype.Service;
+
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class ReportServiceImpl implements ReportService {
+
+    private final OrderMapper orderMapper;
+    /**
+     * 营业额统计
+     * @param begin
+     * @param end
+     * @return
+     */
+    @Override
+    public TurnoverReportVO getTurnoverStatistics(LocalDate begin, LocalDate end) {
+    //存放begin到end的所有日期
+        List<LocalDate> dateList = new ArrayList<>();
+        // 遍历begin到end的所有日期，将其添加到dateList中
+        for (LocalDate date = begin; !date.isAfter(end); date = date.plusDays(1)) {
+            dateList.add(date);
+        }
+        // 遍历dateList，查询每个日期对应的营业额
+        List<Double> turnoverList = new ArrayList<>();
+        for (LocalDate date : dateList) {
+            //查询date日期对应的营业额，营业额是指状态为已完成的订单的金额总和
+            LocalDateTime beginTime = LocalDateTime.of(date, LocalTime.MIN);
+            LocalDateTime endTime = LocalDateTime.of(date, LocalTime.MAX);
+
+            Map map = new HashMap();
+            map.put("begin", beginTime);
+            map.put("end", endTime);
+            map.put("status", Orders.COMPLETED);
+            Double turnover = orderMapper.sumByMap(map);
+            turnover = turnover == null ? 0.0 : turnover;
+            turnoverList.add(turnover);
+        }
+        // 拼接日期列表，格式为：yyyy-MM-dd,yyyy-MM-dd,yyyy-MM-dd
+        String dateListStr = StringUtils.join(dateList, ",");
+        return TurnoverReportVO.builder()
+                .dateList(dateListStr)
+                .turnoverList(StringUtils.join(turnoverList, ","))
+                .build();
+    }
+}
